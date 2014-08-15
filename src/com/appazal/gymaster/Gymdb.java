a87ce099fe7486c287f5f03d87a474dc081307f3
Gazal Garg <gazal24@gmail.com>
2014-08-15 12:37:41 +0530

    Database schema & Helper class added.

---
+++ src/com/appazal/gymaster/Gymdb.java
@@ -0,0 +1,32 @@
+package com.appazal.gymaster;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+public final class Gymdb implements BaseColumns {
+	
+	public Gymdb(){
+		
+	}
+	
+	public static abstract class Muscle implements BaseColumns{
+		public static final String TABLE_NAME = "muscles";
+		public static final String COLUMN_NAME_NAME = "name";
+	}
+	
+	public static abstract class Group implements BaseColumns{
+		public static final String TABLE_NAME = "groups";
+		public static final String COLUMN_NAME_NAME = "name";
+		public static final String COLUMN_NAME_LAST_DATE = "last_date";
+	}
+	
+	public static abstract class Set implements BaseColumns {
+		public static final String TABLE_NAME = "sets";
+		public static final String COLUMN_NAME_MUSCLE = "muscle_id";
+		public static final String COLUMN_NAME_GROUP = "group_id";
+	}
+	
+}
+
---
+++ src/com/appazal/gymaster/GymdbHelper.java
@@ -0,0 +1,100 @@
+package com.appazal.gymaster;
+
+import java.util.Date;
+
+import com.appazal.gymaster.Gymdb.Group;
+import com.appazal.gymaster.Gymdb.Muscle;
+import com.appazal.gymaster.Gymdb.Set;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class GymdbHelper extends SQLiteOpenHelper{
+	
+	private static final String DATABASE_NAME = "gymaster_db";
+	private static final int DATABASE_VERSION = 1;
+
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String COMMA_SEP = ",";
+	private static final String SQL_CREATE_MUSCLES= 
+			"CREATE TABLE " + Muscle.TABLE_NAME + " (" + 
+	    	Muscle._ID + " INTEGER PRIMARY KEY," + COMMA_SEP +
+	    	Muscle.COLUMN_NAME_NAME+ TEXT_TYPE + ")";
+
+	private static final String SQL_DELETE_MUSCLES =
+	    "DROP TABLE IF EXISTS " + Muscle.TABLE_NAME;
+
+	private static final String SQL_CREATE_GROUPS = 
+			"CREATE TABLE " + Group.TABLE_NAME + " (" + 
+			Group._ID + " INTEGER PRIMARY KEY," + COMMA_SEP +
+			Group.COLUMN_NAME_LAST_DATE + " DATE," + COMMA_SEP +
+	    	Group.COLUMN_NAME_NAME+ TEXT_TYPE + ")";
+
+	private static final String SQL_DELETE_GROUPS =
+	    "DROP TABLE IF EXISTS " + Group.TABLE_NAME;
+
+	private static final String SQL_CREATE_SETS = 
+			"CREATE TABLE " + Set.TABLE_NAME + " (" + 
+			Set._ID + " INTEGER PRIMARY KEY," + COMMA_SEP +
+	    	Set.COLUMN_NAME_MUSCLE+ TEXT_TYPE + COMMA_SEP + 
+	    	Set.COLUMN_NAME_GROUP+ TEXT_TYPE + ")";
+
+	private static final String SQL_DELETE_SETS =
+	    "DROP TABLE IF EXISTS " + Set.TABLE_NAME;
+
+	public GymdbHelper(Context context) {
+		super(context, DATABASE_NAME, null , DATABASE_VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(SQL_CREATE_MUSCLES);
+		db.execSQL(SQL_CREATE_GROUPS);
+		db.execSQL(SQL_CREATE_SETS);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL(SQL_DELETE_MUSCLES);
+		db.execSQL(SQL_DELETE_GROUPS);
+		db.execSQL(SQL_DELETE_SETS);
+	}
+	
+	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+       onUpgrade(db, oldVersion, newVersion);
+    }
+	
+	private void insertMuscle(Context context, String name){
+		GymdbHelper mDbHelper = new GymdbHelper(context);
+		SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(Muscle.COLUMN_NAME_NAME, name);
+		db.insert(Muscle.TABLE_NAME, null, values);
+	}
+	
+	private void insertGroup(Context context, String name, String date){
+		GymdbHelper mDbHelper = new GymdbHelper(context);
+		SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(Group.COLUMN_NAME_NAME, name);
+		values.put(Group.COLUMN_NAME_LAST_DATE, date);
+		db.insert(Group.TABLE_NAME, null, values);
+	}
+
+	
+	private void insertSet(Context context, String muscle_id, String group_id){
+		GymdbHelper mDbHelper = new GymdbHelper(context);
+		SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+		ContentValues values = new ContentValues();
+		values.put(Set.COLUMN_NAME_GROUP, group_id);
+		values.put(Set.COLUMN_NAME_MUSCLE, muscle_id);
+		db.insert(Set.TABLE_NAME, null, values);
+	}
+
+}
